ed1ea8a21865b659f03174022ce40f7227f6487d
TQT67 <trangtrinh000000@gmail.com>
2023-02-07 20:10:16 +0700

    style_assets_th2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import style from "./styles.module.css";
 
 function App() {
+  const [numClicked, setNumClicked] = useState(0);
+  const handleClick = () => {
+    setNumClicked((current) => {
+      return (current = current + 1);
+    });
+  };
+  const textStyle = numClicked % 2 === 0 ? style.textBlue : style.textRed;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Click here</button>
+      <h2 className={textStyle}>You clicked {numClicked} times</h2>
+      {/* Dùng Styled-Components */}
+      {/* <StyledText numClicked={numClicked}>You clicked {numClicked} times</StyledText> */}
     </div>
   );
+}
+// Dùng CSS Inline
+{
+  /* <h2
+  style={{
+    color: numClicked % 2 === 0 ? "blue" : "red",
+  }}
+>
+  You clicked {numClicked} times
+</h2>; */
 }
 
 export default App;
---
+++ src/StyledText.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledText = styled.h2`
+  color: ${(props) => {
+    if (props.numClicked % 2 === 0) {
+      return "blue";
+    }
+    return "red";
+  }};
+`;
